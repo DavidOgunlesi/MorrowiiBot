50284733fe7af070ff76ac44db700be3909b100a
David Ogunlesi <david.ogunlesi@yahoo.co.uk>
2023-03-27 12:54:13 +0100

    fixed bad join

--- feature/egirl.py
+++ feature/egirl.py
@@ -357,7 +357,7 @@ def send_morrowii_message_raw(authorName, user_message, conversation):
         # if conversaiton is too long, remove the second oldest message
         if len(conversation) > 10:
             conversation.pop(1)
-        print("[DEV]:",conversation.join("\n"))
+        print("[DEV]:","\n".join(conversation))
 
         conversation.append({'role': "user", 'content': f"[{authorName}]: {user_message} {post_instruction_better}\n"})
         response = ask_chat_gpt(conversation)
